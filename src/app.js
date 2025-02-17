9d51747706f34f30b37615d4ba555fa7723ec7b7
Daniel Santiago Torres Acosta <ruloss29@outlook.es>
2025-02-16 21:24:37 -0500

    Add user delete functionality and update admin panel

--- src/app.js
+++ src/app.js
@@ -2,20 +2,27 @@ const express = require('express');
 const path = require('path');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'login.html'));
 });
+
 app.get('/user', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'user.html'));
 });
 
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'admin.html'));
+});
 
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
+app.use('/api', adminRoutes);
 
 module.exports = app;
