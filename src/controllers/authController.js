16176225cacb49108496d78a93042124f9ae3796
Daniel Santiago Torres Acosta <ruloss29@outlook.es>
2025-02-17 17:14:16 -0500

    Add user registration functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -10,4 +10,14 @@ const loginUser = async (req, res) => {
     }
 };
 
-module.exports = { loginUser };
+const registerUser = async (req, res) => {
+    try {
+        const { username, password, type } = req.body;
+        const result = await authService.register(username, password, type);
+        res.json(result);
+    } catch (error) {
+        res.status(error.status || 500).json({ error: error.message });
+    }
+};
+
+module.exports = { loginUser, registerUser };
