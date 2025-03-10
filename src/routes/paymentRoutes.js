6aad03e971b788f2440488a32c2ab3207ae8b6c9
Daniel Santiago Torres Acosta <ruloss29@outlook.es>
2025-03-10 16:16:46 -0500

    Info error

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -5,7 +5,7 @@ const path = require('path');
 router.get('/payment', (req, res) => {
     const appointmentId = req.query.id;
     if (!appointmentId) {
-        return res.status(400).send('Appointment ID not provided');
+        return res.status(400).send('[ERROR] Appointment ID not provided');
     }
     res.sendFile(path.join(__dirname, '../views/payment.html'));
 });
